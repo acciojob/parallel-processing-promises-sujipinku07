0a1a7a43b47a6080a1f077d3a22e97162463b5d8
sujipinku07 <sujipinku07@gmail.com>
2024-10-16 12:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+//function to download an image
+const loadImage = (image) => {
+return new Promise((resolve, reject) => {
+	const img = new Image();
+	img.src = image.url;
+	img.onload =() => resolve(img); //resolve the promise when the image is loaded
+	img.onerror=() =>
+		reject(new Error('Failed to load image's URL: ${image.url}')); //reject if loading fails
+		
+});
+};
+//Function to handel the click event and download images in parallel
+btn.addEventListener("click", () => {
+	//map the array of image URLs into an array of promises
+	const promises = images.map((image) => loadImage(image));
+
+	//use promise.all to download all images in parallel
+	Promise.all(promises)
+	.then((loadedImages) => {
+		//clear any previous images
+		output.innerHTML = "";
+
+		//Append all successfully loaded images to the output div
+		loadedImages.forEacch((img) => {
+			output.appendChild(img);
+		});
+	})
+	.catch((error) => {
+		//Handle errors if any img fails to load
+		console.error(error.message);
+	});
+});
